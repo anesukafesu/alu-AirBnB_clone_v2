05ec0e807fbb67f5294b152c5c13f15a24cc44e5
Anesu Kafesu <anesukafesu@gmail.com>
2023-10-15 17:06:16 +0200

    1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+"""Python module for packing web_static
+"""
 from datetime import datetime
 from fabric.api import local
 
+
 def do_pack():
+    """Function to pack web_static into .tgz folder
+    """
     now_string = datetime.now().strftime('%Y%M%d%H%M%S')
 
     archive_name = "web_static_{}.tgz".format(now_string)
