abf29cb5394a37fd01f74088570a64bcc02eca97
Anesu Kafesu <anesukafesu@gmail.com>
2023-10-15 17:19:59 +0200

    1-fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,10 +8,17 @@ from fabric.api import local
 def do_pack():
     """Function to pack web_static into .tgz folder
     """
-    now_string = datetime.now().strftime('%Y%M%d%H%M%S')
+    # Creating the datetime string
+    now_string = datetime.now().strftime('%Y%m%d%H%M%S')
 
+    # Generating the archive name
     archive_name = "web_static_{}.tgz".format(now_string)
-    
+
+    # Generating the archive using tar
     local('tar -czvf {} ./web_static'.format(archive_name))
 
-    local('mv ./{} ./versions/{} -p'.format(archive_name, archive_name))
+    # Creating the versions directory if it doesn't already exist
+    local('mkdir versions -p')
+
+    # Moving the archive to the versions directory
+    local('mv ./{} ./versions/{}'.format(archive_name, archive_name))
