e3e5c1d4ea3bbbe2da0d2382ae4e35de3a9f5b14
Anesu Kafesu <anesukafesu@gmail.com>
2023-10-18 23:25:45 +0200

    2 - hbnb route

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Module that creates a web server using Flask
+"""
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    """Index route
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """The HBNB route
+    """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run()
