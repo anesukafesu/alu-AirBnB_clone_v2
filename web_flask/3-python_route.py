1b35b6af26d0108878b9b0df26f9c8ad969dfec6
Anesu Kafesu <anesukafesu@gmail.com>
2023-10-18 23:35:27 +0200

    3: python is cool

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" Module that creates a web server using Flask
+"""
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    """Index route
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """The HBNB route
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c(text):
+    """The C route
+    """
+    return "C " + text.replace('_', ' ')
+
+
+@app.route('/python/<text>')
+def python(text='is cool'):
+    """The Python route
+    """
+    return "Python " + text.replace('_', ' ')
+
+
+if __name__ == "__main__":
+    app.run()
